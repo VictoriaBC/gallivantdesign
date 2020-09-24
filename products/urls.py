bdfc5875d452f5c61076012b137cedcb135b5302
VictoriaBC <victoria.benchivar@gmail.com>
2020-09-24 18:59:51 +0000

    adjusted search, added product pages and categories

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products')
+    path('', views.all_products, name='products'),
+    path('', views.all_products, name='illustrations'),
+    path('', views.all_products, name='services'),
+    path('', views.all_products, name='showcase')
 ]
