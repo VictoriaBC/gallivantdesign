e4274025613142624ebeb593a11bb7dfd63ba637
VictoriaBC <victoria.benchivar@gmail.com>
2020-09-26 18:20:12 +0000

    Added sections to html base and index. Adjusted css background, spacing, font size, added footer

--- products/views.py
+++ products/views.py
@@ -38,4 +38,38 @@ def product_detail(request, product_id):
         'product': product,
     }
 
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
+
+    products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('illustrations'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    context = {
+        'products': products,
+        'search_term': query,
+    }
+
+    return render(request, 'products/illustrations.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'illustrations': product,
+    }
+
+
     return render(request, 'products/product_detail.html', context)
