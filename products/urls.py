9524f40b12e2ad62470bc0825f543bb59ea2746e
VictoriaBC <victoria.benchivar@gmail.com>
2020-09-27 18:28:49 +0000

    Added shopping bag

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.all_products, name='products'),
     path('<product_id>', views.product_detail, name='product_detail'),
-    path('', views.all_products, name='illustrations'),
-    path('', views.all_products, name='services'),
-    path('', views.all_products, name='showcase')
 ]
