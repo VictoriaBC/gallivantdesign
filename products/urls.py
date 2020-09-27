29bc101e9142c77d97ac27bb045691eb5862ad32
VictoriaBC <victoria.benchivar@gmail.com>
2020-09-27 17:20:44 +0000

    Added product details and search query

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
+    path('<product_id>', views.product_detail, name='product_detail'),
     path('', views.all_products, name='illustrations'),
     path('', views.all_products, name='services'),
     path('', views.all_products, name='showcase')
