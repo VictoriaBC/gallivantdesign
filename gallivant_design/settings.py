ebdad5d5c6bd739552654e9507c1d022c6a6087a
VictoriaBC <victoria.benchivar@gmail.com>
2020-10-03 20:38:30 +0000

    Added checkout

--- gallivant_design/settings.py
+++ gallivant_design/settings.py
@@ -43,6 +43,10 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'illustrations',
+    'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -56,6 +60,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'gallivant_design.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -71,11 +77,18 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
--- gallivant_design/urls.py
+++ gallivant_design/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('illustrations/', include('illustrations.urls')),
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
