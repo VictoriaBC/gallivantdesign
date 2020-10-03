09ba4555400073086fd8d184551ee42cb71abce0
VictoriaBC <victoria.benchivar@gmail.com>
2020-10-03 18:35:46 +0000

    Added quantity boxes, adjusted bag html

--- checkout/views.py
+++ checkout/views.py
@@ -6,8 +6,6 @@ from django.conf import settings
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
-from profiles.forms import UserProfileForm
-from profiles.models import UserProfile
 from bag.contexts import bag_contents
 
 import stripe
