ff9d8e272f660c1d8e8b7565ddc9801020943c92
VictoriaBC <victoria.benchivar@gmail.com>
2020-10-11 19:57:16 +0000

    adjusted download

--- profiles/views.py
+++ profiles/views.py
@@ -48,3 +48,14 @@ def order_history(request, order_number):
     }
 
     return render(request, template, context)
+
+def download_file(request):
+    # fill these variables with real values
+    fl_path = '/download_file/path'
+    filename = 'downloaded_file_name.extensio'
+
+    fl = open(fl_path, 'r')
+    mime_type, _ = mimetypes.guess_type(fl_path)
+    response = HttpResponse(fl, content_type=mime_type)
+    response['Content-Disposition'] = "attachment; filename=%s" % filename
+    return response(request, template, context)
