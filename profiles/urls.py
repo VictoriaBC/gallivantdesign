12872b2af5300d8daaeb283743e5476e12930126
VictoriaBC <victoria.benchivar@gmail.com>
2020-10-24 12:55:44 +0000

    Adjusted css and html

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('order_history/<order_number>', views.order_history, name='order_history'),
 	path('contact-me/', views.ContactMe.as_view(), name='contact-me'),
-	path('download_image/', views.order_history, name='download_image'),
 ]
