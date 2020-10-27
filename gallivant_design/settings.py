f199bddcc56c030052321406305b728e393a94df
VictoriaBC <victoria.benchivar@gmail.com>
2020-10-27 21:27:14 +0000

    Connected github to heroku

--- gallivant_design/settings.py
+++ gallivant_design/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '51HIwwYKvrvBCeqcksDUZdKzloA46CUohJT7jjgyZsuofoDR7oDwGzycu0la49dD0ZySsftLfdPSqujSMjguNKQlX00euOXCjUp'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['gallivantdesign.herokuapp.com', 'localhost']
 
